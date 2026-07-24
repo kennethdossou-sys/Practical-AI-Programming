ed0f1147ef9e3f87203848c34a141d04fd1e75c5
vertexacademy221-coder <vertexacademy221@gmail.com>
2026-07-24 12:30:01 +0000

    train the perceptron

--- neural-network/app/src/main/java/com/vertexacademy/neuralnetwork/Neuron.java
+++ neural-network/app/src/main/java/com/vertexacademy/neuralnetwork/Neuron.java
@@ -9,9 +9,11 @@
  *              * Getter :
  *                  * getWeights () : double[]
  *                  * getBias() : double
+ *                  * getLearningRate() : double
  *              * Setter :
- *                  * setWeights(double[] weights),
- *                  * setBias(double b)
+ *                  * setWeights(double[]),
+ *                  * setBias(double)
+ *                  * setLearningRate(double)
  *              * feed(double[] inputs) : int 
  *              
 */
@@ -37,6 +39,7 @@ public class Neuron {
     private double[] weights;
     private double bias;
     private double output;
+    private double learningRate;
 
 
     /*
@@ -54,7 +57,11 @@ public class Neuron {
             this.weights[i] = random.nextDouble() * 2 - 1;
         }
         this.bias = random.nextDouble() * 2 - 1;
-    }
+
+        // Fixer une valeur de coeficient d'apprentissage tres proche
+        // de zero
+        this.learningRate = 0.1;
+    }   
 
     /*
     *   Fonction d'activation du neuron.
@@ -75,28 +82,61 @@ public class Neuron {
         return (this.output > 0) ? 1 : 0;
     }
 
+    /*
+    *   Methode d'entrainement d'un neurone
+    */
+    public void train(double[] inputs, double desiredOutput) 
+    {
+        var output = this.feed(inputs);
+
+        // theError represente la difference entre la valeur de sortie 
+        // souhaitee et la valeur de sortie actuelle
+        var theError = desiredOutput - output;
+
+        // Cette boucle permet d'ajuster et mettre a jour les poids ainsi du perceptron
+        for (int i = 0; i < inputs.length; i++)
+        {
+            var newWeights = this.weights[i] + (this.learningRate * theError * inputs[i]);
+            this.weights[i] = newWeights;
+        }
+        this.bias += (this.learningRate * theError);
+    }
+
     /*[Getter]*/
     
     public double[] getWeights() {return weights;}
     public double getBias() {return bias;}
+    public double getLearningRate() {return learningRate;}
 
     /*[Setter]*/
 
     public void setWeights(double[] weights) { this.weights = weights;}
     public void setBias(double bias) { this.bias = bias;}
+    public void setLearningRate(double learningRate) {this.learningRate = learningRate;}
 
-    public String getGreeting() {
-        return "Hello World!";
-    }
 
     public static void main(String[] args) {
 
-        double[] data = {5.0, 2.0};
-        Neuron n1 = new Neuron(2);
+        double[] data = {0.0, 1.0};
+        Neuron n = new Neuron(2);
 
-        System.out.println("neuron bias = " + n1.getBias());
-        System.out.println("neuron weights : [" + n1.getWeights()[0]
-                        + ", " + n1.getWeights()[1] + "]");
-        System.out.println("neuron fires " + n1.feed(data));
+        n.setWeights(new double[]{-1.0, -1.0});
+        n.setBias(2.0);
+
+        System.out.println("\n\nBefore training...");
+
+        System.out.println("neuron bias = " + n.getBias());
+        System.out.println("neuron weights : [" + n.getWeights()[0]
+                        + ", " + n.getWeights()[1] + "]");
+        
+       
+        n.train(data, 0);
+
+        System.out.println("\n\nAfter training...");
+
+        System.out.println("neuron bias = " + n.getBias());
+        System.out.println("neuron weights : [" + n.getWeights()[0]
+                        + ", " + n.getWeights()[1] + "]");
+        System.out.println("neuron fires " + n.feed(data));
     }
 }
